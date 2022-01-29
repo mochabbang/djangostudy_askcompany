41a9354cc9ca9fc7c98d487d5116ef2ad9b69991
mochabbang <sungmin9036@gmail.com>
2022-01-29 09:03:18 +0900

    장고기본CBV API(BaseView)-22.01.29

--- instagram/urls.py
+++ instagram/urls.py
@@ -2,8 +2,10 @@ from django.urls import  path
 
 from . import views
 
+app_name = 'instagram'
+
 urlpatterns = [
-    path('', views.post_list),
+    path('', views.post_list, name='post_list'),
     path('<int:pk>/', views.post_detail),    # 포스트(게시판목록의 상세) 내용 확인하는 url
     #re_path(r'(?P<pk>\d+)/$', views.post_detail),
 ]
